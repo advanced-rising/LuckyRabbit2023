56aef2f6aaf0219db32115427bd580f60e531e19
risingcore <vcjsm2283@gmail.com>
2023-03-16 21:10:33 +0900

    update : Button css

--- src/components/button/DefaultButton.tsx
+++ src/components/button/DefaultButton.tsx
@@ -1,13 +1,15 @@
+import { CSSObject } from '@emotion/react';
 import { Button } from 'components/ui/Element';
 import React from 'react';
 
 interface Props {
+  sx?: CSSObject;
   children: React.ReactNode;
 }
 
-const DefaultButton = ({ children }: Props) => {
+const DefaultButton = ({ children, sx }: Props) => {
   return (
-    <Button component={'button'} sx={{ width: '330px', height: '50px' }}>
+    <Button component={'button'} sx={{ width: '100%', maxWidth: 330, height: '50px', ...sx }}>
       {children}
     </Button>
   );
