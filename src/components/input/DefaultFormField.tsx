52600c0ceda65b964c4e985f768b735a82f5a37d
Kim Seong Hun <justfix010@gmail.com>
2023-03-18 01:59:45 +0900

    Feat : SignUp

--- src/components/input/DefaultFormField.tsx
+++ src/components/input/DefaultFormField.tsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import { Box, Input, Label } from 'components/ui/Element';
+import React, { InputHTMLAttributes } from 'react';
+import { Colors } from 'utils/Constants';
+import { CSSObject } from '@emotion/serialize';
 
-const DefaultFormField = () => {
-  return <div>DefaultFormField</div>;
+interface Props {
+  label: string;
+  sx?: CSSObject;
+  containerStyle?: CSSObject;
+}
+
+const DefaultFormField = ({ containerStyle, label, sx, ...rest }: Props & InputHTMLAttributes<HTMLInputElement>) => {
+  return (
+    <Box component={'div'} sx={{ display: 'block', padding: '0 30px 40px', ...containerStyle }}>
+      <Label component={'label'} htmlFor={rest.id}>
+        {label}
+      </Label>
+      <Input
+        id={rest.id}
+        component={'input'}
+        sx={{
+          width: '100%',
+          height: '32px',
+          backgroundColor: 'transparent',
+          borderBottom: `1px solid ${Colors.mainTitleColor}`,
+          marginTop: '16px',
+          paddingLeft: '2px',
+          ...sx,
+        }}
+        {...rest}
+      />
+    </Box>
+  );
 };
 
 export default DefaultFormField;
