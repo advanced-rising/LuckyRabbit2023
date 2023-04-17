fb51d557dfbee659184d60bdc40379f51feb9a25
risingcore <vcjsm2283@gmail.com>
2023-04-17 23:52:15 +0900

    add : user dto

---
+++ server/src/user/user.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from 'src/auth/get-user.decorator';
+import { User } from 'src/auth/user.entity';
+import { UserService } from './user.service';
+
+@Controller('user')
+@UseGuards(AuthGuard('jwt'))
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get('/me')
+  me(@GetUser() user: User): Promise<Partial<User>> {
+    return this.userService.me(user);
+  }
+}
---
+++ server/src/user/user.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/auth/user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async me(user: User): Promise<Partial<User>> {
+    const dto = await this.usersRepository.findOne({ where: { id: user.id } });
+
+    if (!dto) {
+      throw new NotFoundException(`User with ID "${user.id}" not found`);
+    }
+
+    return {
+      username: dto.username,
+      email: dto.email,
+      id: dto.id,
+    };
+  }
+}
