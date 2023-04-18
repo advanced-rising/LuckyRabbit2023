e7b8f7abf6d3a07d9772f226aa844ecfbc499bf4
risingcore <vcjsm2283@gmail.com>
2023-04-18 23:33:15 +0900

    fixed : axios

--- website/src/apis/userApi.ts
+++ website/src/apis/userApi.ts
@@ -1,8 +1,13 @@
 import axiosInstance from 'utils/axios';
 import MeDto from 'types/MeDto';
+import { JWT_TOKEN_NAME } from 'utils/Constants';
 
 const me = async (): Promise<MeDto> => {
-  const { data } = await axiosInstance.get('/v1/user/me');
+  const accessToken = localStorage.getItem(JWT_TOKEN_NAME);
+
+  const { data } = await axiosInstance.get('/v1/user/me', {
+    headers: { Authorization: `Bearer ${accessToken}` },
+  });
   return data;
 };
 
