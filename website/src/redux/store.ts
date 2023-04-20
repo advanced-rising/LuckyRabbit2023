6c86f6e924fc771df9b668c0cde2af2f9e8bf916
risingcore <vcjsm2283@gmail.com>
2023-04-20 23:27:48 +0900

    fixed : redux middleware

--- website/src/redux/store.ts
+++ website/src/redux/store.ts
@@ -11,6 +11,10 @@ const rootReducer = combineReducers({
 
 export const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export type AppDispatch = typeof store.dispatch;
